f75467667e9b05593ccbf29b25288c387443b7e5
Randysman <134296647+Randysman@users.noreply.github.com>
2025-07-31 20:41:47 +0300

    update project

--- main/views.py
+++ main/views.py
@@ -1,25 +1,11 @@
-from zoneinfo import available_timezones
 
 from django.shortcuts import render, get_object_or_404
-from unicodedata import category
+from django.views.generic import ListView
 
 from .models import Category, Product
 
 
-def product_list(request, category_slug=None):
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
-
-    category = None
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
-
-    return render(request, 'main/product/list.html', {'category':category, 'categories':categories, 'products':products})
-
-
-def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    related_products = Product.objects.filter(category=category.category, available=True).exlude(id=product.id)[:4]
-
-    return render(request, 'main/product/detail.html', {'product':product, 'related_products':related_products})
+class Product(ListView):
+    model = Product
+    queryset = Product.objects.all()
+    template_name = "main/product/list.html"
