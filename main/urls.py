4c8cfc5aded4a8b57d0e47df9ada27920c2e8813
Randysman <134296647+Randysman@users.noreply.github.com>
2025-08-02 18:06:13 +0300

    update styles

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', ProductView.as_view(), name='product_main'),
-    path('shop', ProductListView.as_view(), name='product_list'),
+    path('shop/', ProductListView.as_view(), name='product_list'),
     path('shop/category/<slug:category_slug>', ProductListView.as_view(), name='product_list_by_category')
 
 ]
